3a27ca129ab9b8581a1397b1292875f4f5e65874
Jonathan Prince <jonathan.prince@gmail.com>
2014-10-06 11:28:45 +0200

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Jonathan Prince on 06/10/14.
+//  Copyright (c) 2014 Jonathan Prince. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
